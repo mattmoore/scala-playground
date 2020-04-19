bc9279d86c7dc904ce11d856ae812ad8bd8af51f
Matt Moore <matt@mattmoore.io>
2020-04-19 15:56:33 -0500

    Add proper range checking on speed limit types.

--- src/test/scala/playground/refined/SpeedLimitSpec.scala
+++ src/test/scala/playground/refined/SpeedLimitSpec.scala
@@ -1,23 +1,24 @@
 package playground.refined
 
 import eu.timepit.refined.api.{Refined, RefinedTypeOps}
+import eu.timepit.refined.boolean.{And, Or}
 import eu.timepit.refined.numeric._
-
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers
 
 class SpeedLimitSpec extends AnyFunSpec with Matchers {
   // Define refined type to represent speeds that fall at or below the legal speed limit of 60
-  type LegalSpeedLimit = Int Refined LessEqual[60]
-  object LegalSpeedLimit extends RefinedTypeOps[LegalSpeedLimit, Int]
+  type LegalSpeed = Int Refined (Greater[0] And LessEqual[60])
+  object LegalSpeed extends RefinedTypeOps[LegalSpeed, Int]
+
   // Define refined type to represent speeds that fall above the legal speed limit of 60
-  type IllegalSpeedLimit = Int Refined Greater[60]
-  object IllegalSpeedLimit extends RefinedTypeOps[IllegalSpeedLimit, Int]
+  type IllegalSpeed = Int Refined (Less[0] Or Greater[60])
+  object IllegalSpeed extends RefinedTypeOps[IllegalSpeed, Int]
 
   describe("Refinement types + pattern matching to determine whether to issue a speeding ticket.") {
     def checkSpeed(speed: Int) = speed match {
-      case LegalSpeedLimit(speed) => s"Your speed is $speed. You're fine. Have a good day, citizen!"
-      case IllegalSpeedLimit(speed) => s"Your speed is $speed. You're speeding! Ticket issued!"
+      case LegalSpeed(speed) => s"Your speed is $speed. You're fine. Have a good day, citizen!"
+      case IllegalSpeed(speed) => s"Your speed is $speed. You're speeding! Ticket issued!"
     }
 
     describe("when speed limit is under the limit") {
