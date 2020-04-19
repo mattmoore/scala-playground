591921dd3ab0fa226475fa6e5c12442f8498b3ec
Matt Moore <matt@mattmoore.io>
2020-04-19 11:35:37 -0500

    Add more refined types example, including custom type.

--- src/test/scala/playground/refined/RefinedSpec.scala
+++ src/test/scala/playground/refined/RefinedSpec.scala
@@ -2,9 +2,8 @@ package playground.refined
 
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
-
 import eu.timepit.refined._
-import eu.timepit.refined.api.Refined
+import eu.timepit.refined.api.{Refined, RefinedTypeOps}
 import eu.timepit.refined.auto._
 import eu.timepit.refined.numeric._
 
@@ -12,5 +11,21 @@ class RefinedSpec extends AnyFlatSpec with Matchers {
   "Int Refined Positive" should "restrict integers to positives" in {
     val i1: Int Refined Positive = 5
     i1.value shouldEqual 5
+  }
+
+  "refineMV" should "restrict integers to positives, while inferring the value type" in {
+    refineMV[Positive](5).value shouldBe 5
+  }
+
+  "refineV" should "behave the same as refineMV, but return an Either monad" in {
+    val result = refineV[Positive](5)
+    result.map(refined => refined.value) shouldBe Right(5)
+  }
+
+  "Custom refined type" should "allow us to define our own types with predicates" in {
+    type PositiveInt = Int Refined Positive
+    object PositiveInt extends RefinedTypeOps[PositiveInt, Int]
+    val result: Either[String, PositiveInt] = PositiveInt.from(5)
+    result.map(refined => refined.value) shouldBe Right(5)
   }
 }
