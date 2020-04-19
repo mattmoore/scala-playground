be5efe38652aa44acf07b72899558031615b3a12
Matt Moore <matt@mattmoore.io>
2020-04-19 10:02:06 -0500

    Add Int Refined Positive example.

--- src/test/scala/playground/refined/RefinedSpec.scala
+++ src/test/scala/playground/refined/RefinedSpec.scala
@@ -2,10 +2,15 @@ package playground.refined
 
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
-import playground.Hello
+
+import eu.timepit.refined._
+import eu.timepit.refined.api.Refined
+import eu.timepit.refined.auto._
+import eu.timepit.refined.numeric._
 
 class RefinedSpec extends AnyFlatSpec with Matchers {
-  "The Hello object" should "say hello" in {
-    Hello.greeting shouldEqual "hello"
+  "Int Refined Positive" should "restrict integers to positives" in {
+    val i1: Int Refined Positive = 5
+    i1.value shouldEqual 5
   }
 }
