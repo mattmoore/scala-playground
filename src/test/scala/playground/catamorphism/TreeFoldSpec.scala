5fcb2e72d5db568ab2aef2d9e1cd12a75cc90335
Matt Moore <matt@mattmoore.io>
2020-05-21 11:27:11 -0500

    Add curried fold function to tree catamorphism example.

--- src/test/scala/playground/catamorphism/TreeFoldSpec.scala
+++ src/test/scala/playground/catamorphism/TreeFoldSpec.scala
@@ -7,16 +7,16 @@ class TreeFoldSpec extends AnyFunSpec with Matchers {
   describe("folds used with trees") {
     sealed trait Tree
     case object Leaf extends Tree
-    case class Node(value: Int, left: Tree, right: Tree) extends Tree
+    case class Node(left: Tree, value: Int, right: Tree) extends Tree
 
     describe("sum as a recursive function") {
       def sum(tree: Tree): Int = tree match {
         case Leaf => 0
-        case Node(value, left, right) => value + sum(left) + sum(right)
+        case Node(left, value, right) => sum(left) + value + sum(right)
       }
 
       describe("with a tree containing a value and two leafs") {
-        val tree = Node(1, Leaf, Leaf)
+        val tree = Node(Leaf, 1, Leaf)
 
         it("should sum all the node values") {
           sum(tree) shouldBe 1
@@ -24,7 +24,7 @@ class TreeFoldSpec extends AnyFunSpec with Matchers {
       }
 
       describe("with a tree containing a value and right and left nodes") {
-        val tree = Node(1, Node(1, Leaf, Leaf), Node(1, Leaf, Leaf))
+        val tree = Node(Node(Leaf, 1, Leaf), 1, Node(Leaf, 1, Leaf))
 
         it("should sum all the node values") {
           sum(tree) shouldBe 3
@@ -33,15 +33,15 @@ class TreeFoldSpec extends AnyFunSpec with Matchers {
     }
 
     describe("sum using a fold allows for abstracting the operation `f` to perform") {
-      def fold(zero: Int, f: (Int, Int, Int) => Int)(tree: Tree): Int = tree match {
-        case Leaf => zero
-        case Node(value, left, right) => f(value, fold(zero, f)(left), fold(zero, f)(right))
+      def fold(start: Int, f: (Int, Int, Int) => Int)(tree: Tree): Int = tree match {
+        case Leaf => start
+        case Node(left, value, right) => f(value, fold(start, f)(left), fold(start, f)(right))
       }
 
       def sum(tree: Tree): Int = fold(0, (a: Int, b: Int, c: Int) => a + b + c)(tree)
 
       describe("with a tree containing a value and two leafs") {
-        val tree = Node(1, Leaf, Leaf)
+        val tree = Node(Leaf, 1, Leaf)
 
         it("should sum all the node values") {
           sum(tree) shouldBe 1
@@ -49,7 +49,32 @@ class TreeFoldSpec extends AnyFunSpec with Matchers {
       }
 
       describe("with a tree containing a value and right and left nodes") {
-        val tree = Node(1, Node(1, Leaf, Leaf), Node(1, Leaf, Leaf))
+        val tree = Node(Node(Node(Node(Leaf, 1, Leaf ), 1, Node(Leaf, 8, Leaf)), 1, Leaf), 1, Node(Leaf, 1, Leaf))
+
+        it("should sum all the node values") {
+          sum(tree) shouldBe 13
+        }
+      }
+    }
+
+    describe("sum using fold with currying and function `f`") {
+      def fold(start: Int)(f: (Int, Int, Int) => Int)(tree: Tree): Int = tree match {
+        case Leaf => start
+        case Node(left, value, right) => f(value, fold(start)(f)(left), fold(start)(f)(right))
+      }
+
+      def sum(tree: Tree): Int = fold(0)((a: Int, b: Int, c: Int) => a + b + c)(tree)
+
+      describe("with a tree containing a value and two leafs") {
+        val tree = Node(Leaf, 1, Leaf)
+
+        it("should sum all the node values") {
+          sum(tree) shouldBe 1
+        }
+      }
+
+      describe("with a tree containing a value and right and left nodes") {
+        val tree = Node(Node(Leaf, 1, Leaf), 1, Node(Leaf, 1, Leaf))
 
         it("should sum all the node values") {
           sum(tree) shouldBe 3
