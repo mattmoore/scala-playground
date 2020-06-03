93d3dbdd2228c9e1b7407a3ef8738ff12817eb64
Matt Moore <matt@mattmoore.io>
2020-06-03 08:00:15 -0500

    Use implicit class.

--- src/test/scala/playground/implicits/ExtensionMethodSpec.scala
+++ src/test/scala/playground/implicits/ExtensionMethodSpec.scala
@@ -5,11 +5,9 @@ import org.scalatest.matchers.should.Matchers
 
 class ExtensionMethodSpec extends AnyFunSpec with Matchers {
   describe("extension methods via implicits") {
-    class RichInt(i: Int) {
+    implicit class RichInt(i: Int) {
       def square = i * i
     }
-
-    implicit def richInt(i: Int) = new RichInt(i)
 
     it("makes int have a square method") {
       2.square shouldBe 4
