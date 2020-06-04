313f98bf9d842ae84453d62437562fd0eb405edf
Matt Moore <matt@mattmoore.io>
2020-06-04 05:03:36 -0500

    Add coercion example for loading a user.

--- src/test/scala/playground/implicits/CoercionSpec.scala
+++ src/test/scala/playground/implicits/CoercionSpec.scala
@@ -4,12 +4,22 @@ import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers
 
 class CoercionSpec extends AnyFunSpec with Matchers {
-  describe("coercion via implicits") {
+  describe("type coercion via implicits for string to int conversion") {
     implicit def stringToInt(string: String) = string.toInt
 
-    it("can automatically convert string to a number, based on matching types with stringToInt") {
+    it("converts a string to a number, based on matching types with stringToInt") {
       val x: Int = "5"
       x shouldBe 5
+    }
+  }
+
+  describe("type coercion via implicit for loading a person record") {
+    case class User(val id: Int, val username: String)
+    implicit def loadUser(userId: Int): User = User(userId, "mattmoore")
+
+    it("can load a user for a given user ID") {
+      val user: User = 1
+      user shouldBe User(1, "mattmoore")
     }
   }
 }
