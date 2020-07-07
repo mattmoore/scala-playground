84d4d8a0b339aa0dc88f62e100463fc03746fe05
Matt Moore <matt@mattmoore.io>
2020-07-07 09:02:38 -0500

    Add another example of pimp my library (extension method) pattern in scala.

--- src/test/scala/io/mattmoore/scala/playground/implicits/ExtensionMethodSpec.scala
+++ src/test/scala/io/mattmoore/scala/playground/implicits/ExtensionMethodSpec.scala
@@ -4,6 +4,18 @@ import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers
 
 class ExtensionMethodSpec extends AnyFunSpec with Matchers {
+  describe("add a method to an existing class without modifying the class source") {
+    case class Person(val firstName: String, val lastName: String)
+
+    implicit class ExtendedPerson(p: Person) {
+      def greet = s"Hello, ${p.firstName} ${p.lastName}!"
+    }
+
+    it("now has a greet method") {
+      Person("Matt", "Moore").greet shouldBe "Hello, Matt Moore!"
+    }
+  }
+
   describe("extension methods via implicits") {
     implicit class RichInt(i: Int) {
       def square = i * i
