e297d3dbd034037432181e39a2bd5078e1287de6
Matt Moore <matt@mattmoore.io>
2020-11-21 05:23:32 -0600

    Alphabetize dependencies.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,14 +2,16 @@ import sbt._
 
 object Dependencies {
 
+  object Akka {
+    lazy val stream = "com.typesafe.akka" %% "akka-stream" % "2.6.6"
+    lazy val actor = "com.typesafe.akka" %% "akka-actor" % "2.6.6"
+    lazy val actorTestkitTyped = "com.typesafe.akka" %% "akka-actor-testkit-typed" % "2.6.6"
+    lazy val protobufV3 = "com.typesafe.akka" %% "akka-protobuf-v3" % "2.6.6"
+  }
+
   object Cats {
     lazy val core = "org.typelevel" %% "cats-core" % "2.0.0"
     lazy val effect = "org.typelevel" %% "cats-effect" % "2.0.0"
-  }
-
-  object Zio {
-    lazy val zio = "dev.zio" %% "zio" % "1.0.0-RC18-2"
-    lazy val streams = "dev.zio" %% "zio-streams" % "1.0.0-RC18-2"
   }
 
   object Refined {
@@ -25,14 +27,13 @@ object Dependencies {
     lazy val shapeless = "eu.timepit" %% "refined-shapeless" % "0.9.13"
   }
 
-  object Akka {
-    lazy val stream = "com.typesafe.akka" %% "akka-stream" % "2.6.6"
-    lazy val actor = "com.typesafe.akka" %% "akka-actor" % "2.6.6"
-    lazy val actorTestkitTyped = "com.typesafe.akka" %% "akka-actor-testkit-typed" % "2.6.6"
-    lazy val protobufV3 = "com.typesafe.akka" %% "akka-protobuf-v3" % "2.6.6"
-  }
-
   object Testing {
     lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.1.1" % Test
   }
+
+  object Zio {
+    lazy val zio = "dev.zio" %% "zio" % "1.0.0-RC18-2"
+    lazy val streams = "dev.zio" %% "zio-streams" % "1.0.0-RC18-2"
+  }
+
 }
