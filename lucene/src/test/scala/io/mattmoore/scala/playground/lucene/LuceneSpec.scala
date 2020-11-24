00c0fc04ea10aeba46ac1cc3846efae0a86e3a8f
Matt Moore <matt@mattmoore.io>
2020-11-24 16:12:11 -0600

    Remove java io stuff.

--- lucene/src/test/scala/io/mattmoore/scala/playground/lucene/LuceneSpec.scala
+++ lucene/src/test/scala/io/mattmoore/scala/playground/lucene/LuceneSpec.scala
@@ -1,7 +1,5 @@
 package io.mattmoore.scala.playground.lucene
 
-import java.nio.file.Paths
-
 import com.outr.lucene4s.DirectLucene
 import com.outr.lucene4s.query.Sort
 import org.scalatest.funspec.AnyFunSpec
@@ -9,23 +7,20 @@ import org.scalatest.matchers.should.Matchers
 
 class LuceneSpec extends AnyFunSpec with Matchers {
   describe("indexing a document") {
-    val directory = Paths.get("index")
     val lucene = new DirectLucene(Nil)
 
     val name = lucene.create.field[String]("name")
     val address = lucene.create.field[String]("address")
 
-    lucene.doc().fields(name("John Doe"), address("123 Somewhere Rd.")).index()
-    lucene.doc().fields(name("Jane Doe"), address("123 Somewhere Rd.")).index()
+    lucene.doc().fields(name("John"), address("123 Somewhere Rd.")).index()
+    lucene.doc().fields(name("Jane"), address("123 Somewhere Rd.")).index()
 
-    describe("searching the index") {
+    describe("return all results in the index") {
       val paged = lucene.query().sort(Sort(name)).search()
-      paged.results.foreach { searchResult =>
-        println(s"Name: ${searchResult(name)}, Address: ${searchResult(address)}")
-      }
 
       it("creates a new index and returns search results") {
-        paged.results.map(_ (name)) shouldBe Vector("Jane Doe", "John Doe")
+        paged.total shouldBe 2
+        paged.results.map(_ (name)) shouldBe Vector("Jane", "John")
       }
     }
   }
