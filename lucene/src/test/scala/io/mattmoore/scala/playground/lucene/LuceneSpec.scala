a1380efe240e2b89a1da92b439fb8078dcf1afa1
Matt Moore <matt@mattmoore.io>
2020-11-25 01:23:03 -0600

    Add keyword search example.

--- lucene/src/test/scala/io/mattmoore/scala/playground/lucene/LuceneSpec.scala
+++ lucene/src/test/scala/io/mattmoore/scala/playground/lucene/LuceneSpec.scala
@@ -1,13 +1,14 @@
 package io.mattmoore.scala.playground.lucene
 
+import com.outr.lucene4s.keyword.KeywordIndexing
 import com.outr.lucene4s.query.Sort
 import com.outr.lucene4s.{DirectLucene, wildcard}
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers
 
 class LuceneSpec extends AnyFunSpec with Matchers {
-  describe("indexing a document") {
-    val lucene = new DirectLucene(Nil)
+  describe("full text search") {
+    val lucene = new DirectLucene(Nil, defaultFullTextSearchable = true)
 
     val name = lucene.create.field[String]("name")
     val address = lucene.create.field[String]("address")
@@ -48,6 +49,28 @@ class LuceneSpec extends AnyFunSpec with Matchers {
       it("returns matching result") {
         paged.total shouldBe 1
         paged.results.map(_ (name)) shouldBe Vector("John Doe")
+      }
+    }
+  }
+
+  describe("keyword search") {
+    val lucene = new DirectLucene(uniqueFields = List("name"), defaultFullTextSearchable = true, autoCommit = true)
+    val keywordIndexing = KeywordIndexing(lucene, "keywords")
+
+    val name = lucene.create.field[String]("name")
+    val address = lucene.create.field[String]("address")
+
+    lucene.doc().fields(name("John Doe"), address("123 Somewhere Rd.")).index()
+    lucene.doc().fields(name("Jane Doe"), address("123 Somewhere Rd.")).index()
+
+    describe("search the index with a query that matches john") {
+      val keywords = keywordIndexing.search("do*")
+
+      it("returns matching result") {
+        keywords.total.value shouldBe 1
+        keywords.results.map(_.word) shouldBe List("Doe")
+        keywords.results.head.score shouldBe 1.0
+        keywords.results.length shouldBe 1
       }
     }
   }
