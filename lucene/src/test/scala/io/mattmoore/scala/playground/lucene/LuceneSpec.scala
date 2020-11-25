b7ba3ba99c22e54674c1f0ab0b87a7acdafe13f1
Matt Moore <matt@mattmoore.io>
2020-11-25 12:41:12 -0600

    Add searchable classes.

--- lucene/src/test/scala/io/mattmoore/scala/playground/lucene/LuceneSpec.scala
+++ lucene/src/test/scala/io/mattmoore/scala/playground/lucene/LuceneSpec.scala
@@ -1,8 +1,10 @@
 package io.mattmoore.scala.playground.lucene
 
+import com.outr.lucene4s.field.Field
 import com.outr.lucene4s.keyword.KeywordIndexing
-import com.outr.lucene4s.query.Sort
-import com.outr.lucene4s.{DirectLucene, wildcard}
+import com.outr.lucene4s.mapper.Searchable
+import com.outr.lucene4s.query.{SearchTerm, Sort}
+import com.outr.lucene4s.{DirectLucene, Stringify, exact, wildcard}
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers
 
@@ -63,7 +65,7 @@ class LuceneSpec extends AnyFunSpec with Matchers {
     lucene.doc().fields(name("John Doe"), address("123 Somewhere Rd.")).index()
     lucene.doc().fields(name("Jane Doe"), address("123 Somewhere Rd.")).index()
 
-    describe("search the index with a query that matches john") {
+    describe("search the index with a query that matches doe") {
       val keywords = keywordIndexing.search("do*")
 
       it("returns matching result") {
@@ -71,6 +73,42 @@ class LuceneSpec extends AnyFunSpec with Matchers {
         keywords.results.map(_.word) shouldBe List("Doe")
         keywords.results.head.score shouldBe 1.0
         keywords.results.length shouldBe 1
+      }
+    }
+  }
+
+  describe("searchable classes") {
+    case class Person(id: Id[Person], name: String, address: String)
+
+    case class Id[T](value: String) {
+      override def toString: String = value
+    }
+
+    trait SearchablePerson extends Searchable[Person] {
+      override def idSearchTerms(person: Person): List[SearchTerm] = List(exact(id(person.id)))
+
+      implicit def stringifyId[T]: Stringify[Id[T]] = Stringify[Id[T]]((s: String) => Id[T](s))
+
+      val id: Field[Id[Person]] = lucene.create.stringifiedField[Id[Person]]("id", fullTextSearchable = false)
+    }
+
+    val lucene = new DirectLucene(uniqueFields = List("name"), defaultFullTextSearchable = true, autoCommit = true)
+    val people = lucene.create.searchable[SearchablePerson]
+
+    people.insert(Person(Id("F1D8E1F9-8E5E-452A-A3CC-FF71C7BF1CFE"), "John Doe", "123 Somewhere Rd.")).index()
+    people.insert(Person(Id("AFD0333E-E697-4ACC-8A08-A79C01250566"), "Jane Doe", "123 Somewhere Rd.")).index()
+
+    describe("search the index with a query that matches Jane") {
+      val paged = people.query().filter("ja*").search()
+
+      it("returns matching result") {
+        paged.total shouldBe 1
+        paged.entries shouldBe Vector(Person(
+          Id("AFD0333E-E697-4ACC-8A08-A79C01250566"),
+          "Jane Doe",
+          "123 Somewhere Rd."
+        ))
+        paged.results.length shouldBe 1
       }
     }
   }
