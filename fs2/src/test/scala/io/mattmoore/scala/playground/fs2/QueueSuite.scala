10be81734aca3c9dda569b648a5389d48d42f8e8
Matt Moore <matt@mattmoore.io>
2021-10-27 16:16:17 -0500

    Some more fs2 stream examples.

--- fs2/src/test/scala/io/mattmoore/scala/playground/fs2/QueueSuite.scala
+++ fs2/src/test/scala/io/mattmoore/scala/playground/fs2/QueueSuite.scala
@@ -1,6 +1,6 @@
 package io.mattmoore.scala.playground.fs2
 
-import cats.effect.IO
+import cats.effect.{IO, Sync}
 import cats.effect.std.Queue
 import cats.effect.testing.scalatest.AsyncIOSpec
 import cats.implicits._
@@ -17,5 +17,29 @@ class QueueSuite extends AsyncFunSuite with AsyncIOSpec with Matchers {
       result <- streamFromQueue.compile.toList
     } yield result
     program.asserting(_ shouldBe List(1, 2, 3))
+  }
+
+  test("Effectful streams") {
+    def getAge(name: String): IO[Int] = IO(name.length)
+    val stream = Stream("John", "Paul", "George", "Ringo")
+    stream.evalMap(getAge).compile.toList.asserting(_ shouldBe List(4, 4, 6, 5))
+  }
+
+  test("Pipes") {
+    def pipe[F[_] : Sync](name: String): Stream[F, Int] => Stream[F, Int] =
+      _.evalTap { index =>
+        Sync[F].delay(
+          println(s"Stage $name processing $index by ${Thread.currentThread().getName}")
+        )
+      }
+
+    Stream.emits(1 to 10000)
+      .covary[IO]
+      .through(pipe("A"))
+      .through(pipe("B"))
+      .through(pipe("C"))
+      .compile
+      .drain
+      .asserting(_ shouldBe ())
   }
 }
