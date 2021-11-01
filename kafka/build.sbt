4b4dea0e921f49f162d3d36077b0e575820942c7
Matt Moore <matt@mattmoore.io>
2021-11-01 00:27:32 -0500

    Add multiple Kafka records.

--- kafka/build.sbt
+++ kafka/build.sbt
@@ -2,11 +2,12 @@ name := "kafka"
 resolvers += "confluent" at "https://packages.confluent.io/maven/"
 
 val log4catsV = "2.1.1"
+val testContainersV = "0.39.8"
 
 libraryDependencies ++= Seq(
   "com.github.fd4s" %% "fs2-kafka" % "2.2.0",
-  "com.dimafeng" %% "testcontainers-scala" % "0.39.8",
-  "com.dimafeng" %% "testcontainers-scala-kafka" % "0.39.8",
+  "com.dimafeng" %% "testcontainers-scala" % testContainersV,
+  "com.dimafeng" %% "testcontainers-scala-kafka" % testContainersV,
   "org.typelevel" %% "cats-effect-testing-scalatest" % "1.3.0" % "test,it",
   "org.typelevel" %% "log4cats-slf4j" % log4catsV,
   "org.typelevel" %% "log4cats-testing" % log4catsV % "test,it",
--- kafka/src/it/scala/io/mattmoore/scala/playground/kafka/KafkaSuite.scala
+++ kafka/src/it/scala/io/mattmoore/scala/playground/kafka/KafkaSuite.scala
@@ -1,48 +1,57 @@
 package io.mattmoore.scala.playground.kafka
 
 import cats.effect.IO
-import cats.effect.testing.scalatest.AsyncIOSpec
+import cats.effect.unsafe.implicits.global
 import com.dimafeng.testcontainers.{ForAllTestContainer, KafkaContainer}
 import fs2.kafka._
-import org.scalatest.funsuite.AsyncFunSuite
+import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 import org.typelevel.log4cats.Logger
 import org.typelevel.log4cats.slf4j.Slf4jLogger
 
 import scala.concurrent.duration.DurationInt
 
-class KafkaSuite extends AsyncFunSuite with AsyncIOSpec with ForAllTestContainer with Matchers {
+class KafkaSuite extends AnyFunSuite with ForAllTestContainer with Matchers {
   override val container: KafkaContainer = KafkaContainer()
 
   implicit val logger: Logger[IO] = Slf4jLogger.getLogger[IO]
 
   test("Producer and consumer example") {
-    val events = fs2.Stream.emits(List(("8f68ba9c-241e-44c3-b552-841612f5ec4e", "This is the first message.")))
+    val events = fs2.Stream.emits(
+      List(
+        ("8f68ba9c-241e-44c3-b552-841612f5ec4e", "This is the first message."),
+        ("fd79c027-a6fa-4f00-86d4-3a9ec90115e6", "A second message."),
+        ("e47b17fb-72c9-4cf2-b6db-226f259b6c6a", "And yet a third message.")
+      )
+    )
 
     val producerSettings = ProducerSettings[IO, String, String]
       .withBootstrapServers(container.bootstrapServers)
       .withAcks(Acks.All)
     val consumerSettings = ConsumerSettings[IO, String, String]
       .withBootstrapServers(container.bootstrapServers)
-      .withAutoOffsetReset(AutoOffsetReset.Earliest)
       .withGroupId("group")
+      .withAutoOffsetReset(AutoOffsetReset.Earliest)
+
     val topic = "topic"
 
-    val result = for {
-      producer <- KafkaProducer.stream(producerSettings)
-      _ <- events
-        .map(m => ProducerRecord(topic, m._1, m._2))
-        .map(m => ProducerRecords.one(m))
-        .evalMap(producer.produce)
-      consumer <- KafkaConsumer
-        .stream(consumerSettings)
-        .evalTap(_.subscribeTo(topic))
-      messages <- consumer.records
+    val results = for {
+      _ <- KafkaProducer.stream(producerSettings).flatMap { producer =>
+          events
+            .map(m => ProducerRecord(topic, m._1, m._2))
+            .map(m => ProducerRecords.one(m))
+            .evalMap(producer.produce)
+        }
+        .compile
+        .drain
+      messages <- KafkaConsumer.stream(consumerSettings).subscribeTo(topic)
+        .records.take(3).compile.toList
     } yield messages
 
-    result.take(1).timeout(10.seconds).compile.lastOrError.asserting { message =>
-      (message.record.key, message.record.value) shouldBe
-        ("8f68ba9c-241e-44c3-b552-841612f5ec4e", "This is the first message.")
-    }
+    results.timeout(10.seconds).unsafeRunSync().map(it => (it.record.key, it.record.value)) shouldBe List(
+      ("8f68ba9c-241e-44c3-b552-841612f5ec4e", "This is the first message."),
+      ("fd79c027-a6fa-4f00-86d4-3a9ec90115e6", "A second message."),
+      ("e47b17fb-72c9-4cf2-b6db-226f259b6c6a", "And yet a third message.")
+    )
   }
 }
