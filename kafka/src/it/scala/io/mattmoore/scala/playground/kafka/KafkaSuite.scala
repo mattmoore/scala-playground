9bfa866b76b05379f2d4b2a7aa4e5eead931ef46
Matt Moore <matt@mattmoore.io>
2021-11-01 20:45:07 -0500

    Dry up the KafkaSpec.

--- kafka/src/it/scala/io/mattmoore/scala/playground/kafka/KafkaSuite.scala
+++ kafka/src/it/scala/io/mattmoore/scala/playground/kafka/KafkaSuite.scala
@@ -17,13 +17,13 @@ class KafkaSuite extends AnyFunSuite with ForAllTestContainer with Matchers {
   implicit val logger: Logger[IO] = Slf4jLogger.getLogger[IO]
 
   test("Producer and consumer example") {
-    val events = fs2.Stream.emits(
-      List(
-        ("8f68ba9c-241e-44c3-b552-841612f5ec4e", "This is the first message."),
-        ("fd79c027-a6fa-4f00-86d4-3a9ec90115e6", "A second message."),
-        ("e47b17fb-72c9-4cf2-b6db-226f259b6c6a", "And yet a third message.")
-      )
+    val messages = List(
+      ("8f68ba9c-241e-44c3-b552-841612f5ec4e", "This is the first message."),
+      ("fd79c027-a6fa-4f00-86d4-3a9ec90115e6", "A second message."),
+      ("e47b17fb-72c9-4cf2-b6db-226f259b6c6a", "And yet a third message.")
     )
+
+    val events = fs2.Stream.emits(messages)
 
     val producerSettings = ProducerSettings[IO, String, String]
       .withBootstrapServers(container.bootstrapServers)
@@ -41,17 +41,12 @@ class KafkaSuite extends AnyFunSuite with ForAllTestContainer with Matchers {
             .map(m => ProducerRecord(topic, m._1, m._2))
             .map(m => ProducerRecords.one(m))
             .evalMap(producer.produce)
-        }
-        .compile
-        .drain
+        }.compile.drain
       messages <- KafkaConsumer.stream(consumerSettings).subscribeTo(topic)
         .records.take(3).compile.toList
     } yield messages
 
-    results.timeout(10.seconds).unsafeRunSync().map(it => (it.record.key, it.record.value)) shouldBe List(
-      ("8f68ba9c-241e-44c3-b552-841612f5ec4e", "This is the first message."),
-      ("fd79c027-a6fa-4f00-86d4-3a9ec90115e6", "A second message."),
-      ("e47b17fb-72c9-4cf2-b6db-226f259b6c6a", "And yet a third message.")
-    )
+    results.timeout(10.seconds).unsafeRunSync()
+      .map(it => (it.record.key, it.record.value)) shouldBe messages
   }
 }
