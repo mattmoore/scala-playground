74db5a9bb54fdf47e78c7fe64830e6fd358e1ade
Matt Moore <matt@mattmoore.io>
2021-11-04 03:22:45 -0500

    Add fs2 RunningState example.

--- catamorphisms/src/test/scala/io/mattmoore/scala/playground/catamorphisms/RunningState.scala
+++ catamorphisms/src/test/scala/io/mattmoore/scala/playground/catamorphisms/RunningState.scala
@@ -1,7 +1,10 @@
 package io.mattmoore.scala.playground.catamorphisms
 
 import cats.data._
+import cats.effect._
+import cats.effect.unsafe.implicits.global
 import cats.implicits._
+import fs2._
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
@@ -9,24 +12,24 @@ class RunningState extends AnyFunSuite with Matchers {
   type Balances = Map[String, BigDecimal]
   type Transaction = (String, BigDecimal)
 
+  val initialBalances = Map(
+    "Alice" -> BigDecimal(100),
+    "Bob" -> BigDecimal(200)
+  )
+
+  val transactions = Vector(
+    ("Alice", BigDecimal(50)),
+    ("Alice", BigDecimal(50)),
+    ("Bob", BigDecimal(50)),
+    ("Bob", BigDecimal(50))
+  )
+
   // Using fold, we get the final balance result, but do not keep the history of transactions in the output.
   test("Running totals using fold with tuples") {
-    def updateBalances(balances: Balances, transactions: List[Transaction]): Map[String, BigDecimal] =
+    def updateBalances(balances: Balances, transactions: Vector[Transaction]): Map[String, BigDecimal] =
       transactions.foldLeft(balances) { case (bal, (name, amount)) =>
         bal + (name -> (bal(name) - amount))
       }
-
-    val initialBalances = Map(
-      "Alice" -> BigDecimal(100),
-      "Bob" -> BigDecimal(200)
-    )
-
-    val transactions = List(
-      ("Alice", BigDecimal(50)),
-      ("Alice", BigDecimal(50)),
-      ("Bob", BigDecimal(50)),
-      ("Bob", BigDecimal(50))
-    )
 
     val result = updateBalances(initialBalances, transactions)
 
@@ -35,7 +38,7 @@ class RunningState extends AnyFunSuite with Matchers {
 
   // Using State monad, we get the final balance calculation, while also maintaining transaction history.
   test("Running totals using State monad") {
-    def updateBalances(transactions: List[Transaction]): State[Balances, List[Transaction]] =
+    def updateBalances(transactions: Vector[Transaction]): State[Balances, Vector[Transaction]] =
       transactions.traverse { transaction =>
         State { balances =>
           val name = transaction._1
@@ -47,21 +50,42 @@ class RunningState extends AnyFunSuite with Matchers {
         }
       }
 
-    val initialBalances = Map(
-      "Alice" -> BigDecimal(100),
-      "Bob" -> BigDecimal(200)
-    )
-
-    val transactions = List(
-      ("Alice", BigDecimal(50)),
-      ("Alice", BigDecimal(50)),
-      ("Bob", BigDecimal(50)),
-      ("Bob", BigDecimal(50))
-    )
-
     val result = updateBalances(transactions).run(initialBalances).value
 
     result._1 shouldBe Map("Alice" -> 0, "Bob" -> 100)
     result._2 shouldBe transactions
+  }
+
+  def updateBalancesStream[F[_]](
+    initialBalances: Balances,
+    transactions: Stream[F, Transaction]
+  ): Stream[F, (Balances, Transaction)] =
+    transactions.mapAccumulate(initialBalances) { case (balances, (name, amount)) =>
+      val newBalance = balances + (name -> (balances(name) - amount))
+      (newBalance, (name, amount))
+    }
+
+  // Using an fs2 Stream we no longer have to load the entire list into memory.
+  test("Running totals with fs2 Stream (Pure)") {
+    val result = updateBalancesStream(initialBalances, Stream.emits(transactions)).compile.toVector
+
+    result shouldBe Vector(
+      (Map("Alice" -> 50, "Bob" -> 200), ("Alice", 50)),
+      (Map("Alice" -> 0, "Bob" -> 200), ("Alice", 50)),
+      (Map("Alice" -> 0, "Bob" -> 150), ("Bob", 50)),
+      (Map("Alice" -> 0, "Bob" -> 100), ("Bob", 50))
+    )
+  }
+
+  // Also using fs2 Stream, but we're lifting to IO instead of Pure.
+  test("Running totals with fs2 Stream (IO)") {
+    val result = updateBalancesStream[IO](initialBalances, Stream.emits(transactions)).compile.toVector.unsafeRunSync
+
+    result shouldBe Vector(
+      (Map("Alice" -> 50, "Bob" -> 200), ("Alice", 50)),
+      (Map("Alice" -> 0, "Bob" -> 200), ("Alice", 50)),
+      (Map("Alice" -> 0, "Bob" -> 150), ("Bob", 50)),
+      (Map("Alice" -> 0, "Bob" -> 100), ("Bob", 50))
+    )
   }
 }
