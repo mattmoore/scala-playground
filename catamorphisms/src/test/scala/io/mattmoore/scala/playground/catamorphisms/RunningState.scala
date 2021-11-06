1d072bf28a43ca3a6b38a886c770cdb040d9448f
Matt Moore <matt@mattmoore.io>
2021-11-05 21:58:44 -0500

    Use Transaction case class instead of tuples.

--- catamorphisms/src/test/scala/io/mattmoore/scala/playground/catamorphisms/RunningState.scala
+++ catamorphisms/src/test/scala/io/mattmoore/scala/playground/catamorphisms/RunningState.scala
@@ -10,7 +10,8 @@ import org.scalatest.matchers.should.Matchers
 
 class RunningState extends AnyFunSuite with Matchers {
   type Balances = Map[String, BigDecimal]
-  type Transaction = (String, BigDecimal)
+
+  case class Transaction(name: String, amount: BigDecimal)
 
   val initialBalances = Map(
     "Alice" -> BigDecimal(100),
@@ -18,18 +19,18 @@ class RunningState extends AnyFunSuite with Matchers {
   )
 
   val transactions = Vector(
-    ("Alice", BigDecimal(50)),
-    ("Alice", BigDecimal(50)),
-    ("Bob", BigDecimal(50)),
-    ("Bob", BigDecimal(50))
+    Transaction("Alice", 50),
+    Transaction("Alice", 50),
+    Transaction("Bob", 50),
+    Transaction("Bob", 50)
   )
 
   def updateBalancesFold(
     balances: Balances,
     transactions: Vector[Transaction]
   ): Map[String, BigDecimal] =
-    transactions.foldLeft(balances) { case (bal, (name, amount)) =>
-      bal + (name -> (bal(name) - amount))
+    transactions.foldLeft(balances) { case (balances, transaction) =>
+      balances + (transaction.name -> (balances(transaction.name) - transaction.amount))
     }
 
   test("Using fold, we get the final balance result, but do not keep the history of transactions in the output.") {
@@ -40,10 +41,10 @@ class RunningState extends AnyFunSuite with Matchers {
   def updateBalancesFoldWithTransactions(
     balances: Balances,
     transactions: Vector[Transaction]
-  ): (Balances, Vector[(String, BigDecimal)]) =
-    transactions.foldLeft((balances, Vector.empty[Transaction])) { case ((bal, trxs), (name, amount)) =>
-      val updatedBalances = bal + (name -> (bal(name) - amount))
-      val updatedTransactions = trxs :+ (name, amount)
+  ): (Balances, Vector[Transaction]) =
+    transactions.foldLeft((balances, Vector.empty[Transaction])) { case ((balances, transactions), transaction) =>
+      val updatedBalances = balances + (transaction.name -> (balances(transaction.name) - transaction.amount))
+      val updatedTransactions = transactions :+ transaction
       (updatedBalances, updatedTransactions)
     }
 
@@ -52,10 +53,10 @@ class RunningState extends AnyFunSuite with Matchers {
     result shouldBe (
       Map("Alice" -> 0, "Bob" -> 100),
       Vector(
-        ("Alice", 50),
-        ("Alice", 50),
-        ("Bob", 50),
-        ("Bob", 50)
+        Transaction("Alice", 50),
+        Transaction("Alice", 50),
+        Transaction("Bob", 50),
+        Transaction("Bob", 50)
       )
     )
   }
@@ -65,47 +66,42 @@ class RunningState extends AnyFunSuite with Matchers {
   ): State[Balances, Vector[Transaction]] =
     transactions.traverse { transaction =>
       State { balances =>
-        val name = transaction._1
-        val amount = transaction._2
-        (
-          balances + (name -> (balances(name) - amount)),
-          transaction
-        )
+        val updatedBalances = balances + (transaction.name -> (balances(transaction.name) - transaction.amount))
+        (updatedBalances, transaction)
       }
     }
 
   test("Using State monad, we get the final balance calculation, while also maintaining transaction history.") {
     val result = updateBalancesStateMonad(transactions).run(initialBalances).value
-    result._1 shouldBe Map("Alice" -> 0, "Bob" -> 100)
-    result._2 shouldBe transactions
+    result shouldBe (Map("Alice" -> 0, "Bob" -> 100), transactions)
   }
 
   def updateBalancesStream[F[_]](
     initialBalances: Balances,
     transactions: Stream[F, Transaction]
   ): Stream[F, (Balances, Transaction)] =
-    transactions.mapAccumulate(initialBalances) { case (balances, (name, amount)) =>
-      val newBalance = balances + (name -> (balances(name) - amount))
-      (newBalance, (name, amount))
+    transactions.mapAccumulate(initialBalances) { case (balances, transaction) =>
+      val updatedBalances = balances + (transaction.name -> (balances(transaction.name) - transaction.amount))
+      (updatedBalances, transaction)
     }
 
   test("Using fs2.Stream.") {
     val result = updateBalancesStream(initialBalances, Stream.emits(transactions)).compile.toVector
     result shouldBe Vector(
-      (Map("Alice" -> 50, "Bob" -> 200), ("Alice", 50)),
-      (Map("Alice" -> 0, "Bob" -> 200), ("Alice", 50)),
-      (Map("Alice" -> 0, "Bob" -> 150), ("Bob", 50)),
-      (Map("Alice" -> 0, "Bob" -> 100), ("Bob", 50))
+      (Map("Alice" -> 50, "Bob" -> 200), Transaction("Alice", 50)),
+      (Map("Alice" -> 0, "Bob" -> 200), Transaction("Alice", 50)),
+      (Map("Alice" -> 0, "Bob" -> 150), Transaction("Bob", 50)),
+      (Map("Alice" -> 0, "Bob" -> 100), Transaction("Bob", 50))
     )
   }
 
   test("Using fs2.Stream, lifted to IO.") {
     val result = updateBalancesStream[IO](initialBalances, Stream.emits(transactions)).compile.toVector.unsafeRunSync
     result shouldBe Vector(
-      (Map("Alice" -> 50, "Bob" -> 200), ("Alice", 50)),
-      (Map("Alice" -> 0, "Bob" -> 200), ("Alice", 50)),
-      (Map("Alice" -> 0, "Bob" -> 150), ("Bob", 50)),
-      (Map("Alice" -> 0, "Bob" -> 100), ("Bob", 50))
+      (Map("Alice" -> 50, "Bob" -> 200), Transaction("Alice", 50)),
+      (Map("Alice" -> 0, "Bob" -> 200), Transaction("Alice", 50)),
+      (Map("Alice" -> 0, "Bob" -> 150), Transaction("Bob", 50)),
+      (Map("Alice" -> 0, "Bob" -> 100), Transaction("Bob", 50))
     )
   }
 
@@ -118,22 +114,22 @@ class RunningState extends AnyFunSuite with Matchers {
       "John" -> 100
     )
     val transactionsA = Vector(
-      ("Alice", BigDecimal(50)),
-      ("Alice", BigDecimal(50)),
-      ("Bob", BigDecimal(50)),
-      ("Bob", BigDecimal(50))
+      Transaction("Alice", BigDecimal(50)),
+      Transaction("Alice", BigDecimal(50)),
+      Transaction("Bob", BigDecimal(50)),
+      Transaction("Bob", BigDecimal(50))
     )
     val transactionsB = Vector(
-      ("Mary", BigDecimal(50)),
-      ("Mary", BigDecimal(50)),
-      ("John", BigDecimal(50)),
-      ("John", BigDecimal(50))
+      Transaction("Mary", BigDecimal(50)),
+      Transaction("Mary", BigDecimal(50)),
+      Transaction("John", BigDecimal(50)),
+      Transaction("John", BigDecimal(50))
     )
     val transactionsC = Vector(
-      ("Anthony", BigDecimal(50)),
-      ("Anthony", BigDecimal(50)),
-      ("Sarah", BigDecimal(50)),
-      ("Sarah", BigDecimal(50))
+      Transaction("Anthony", BigDecimal(50)),
+      Transaction("Anthony", BigDecimal(50)),
+      Transaction("Sarah", BigDecimal(50)),
+      Transaction("Sarah", BigDecimal(50))
     )
 
     val streamA = Stream.emits(transactionsA)
